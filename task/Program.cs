a4490e94b659f6a6b44ea56d290d39711331308d
peekay <pakobaloyi7@gmail.com>
2023-05-23 15:57:36 +0200

    Add project files.

---
+++ task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using task;
+
+namespace AssessmentTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select 1: Q1"+"\n"+"2: Q2" + "\n" +  "3: Q3" + "\n" + "4: Q4" + "\n" + "5: Q5");
+            int response= int.Parse(Console.ReadLine());
+            
+            switch (response)
+            {
+                case 1:
+                      Utilities.Q1();
+                      break;
+                case 2:
+                      Utilities.Q2();
+                      break;
+                case 3:
+                    break;
+                case 4:
+                    Utilities.Q1();
+                    break;
+                case 5:
+                    Utilities.Q1();
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ task/Utilities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    internal class Utilities
+    {
+        public static void Q1()
+        {
+            Console.WriteLine("Enter a string");
+            var input = Console.ReadLine();
+            var convertedText = input.ToCharArray();
+            Array.Reverse(convertedText);
+            Console.WriteLine($"Input : {input}");
+            Console.WriteLine($"Output :" + new string(convertedText));
+        }
+        public static void Q2()
+        {
+            Console.WriteLine("Enter a string");
+            var input = Console.ReadLine();
+            var convertedText = input.ToCharArray();
+            Array.Reverse(convertedText);
+
+            if (input.Equals(new string(convertedText)))
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine(" is not Palindrome");
+
+            }
+
+        }
+        public static string ReversedWordsInSentence(string input)
+        {
+            string[] convertedString = input.Split(' ');
+            Array.Reverse(convertedString);
+            string newSentence = string.Join(" ", convertedString);
+            return newSentence;
+        }
+        public static void Q4()
+        {
+            Console.WriteLine("Enter a string");
+            var input = Console.ReadLine();
+            var convertedText = input.ToCharArray();
+            Array.Reverse(convertedText);
+            Console.WriteLine(ReversedWordsInSentence(new String(convertedText)));
+        }
+
+        
+    }
+}
